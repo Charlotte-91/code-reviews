f2c3eca70eade1802f41b1849ffc9b34038ee594
Charlotte Smallwood-Smith <c.smallwood.smith@gmail.com>
2020-09-18 09:38:08 +0100

    Made passing test for grade count, created failing test for gradeResult

--- Review-1/lib/grades.rb
+++ Review-1/lib/grades.rb
@@ -2,7 +2,16 @@ class Grades
 
 
   def input(grades)
-    return grades
+    grades = grades.delete(' ')
+    gradeArr = grades.split(',')
   end
+
+   def gradeCount(gradeArr)
+    result = Hash.new(0)
+    gradeArr.each { |grade| result[grade] += 1 }
+    return result
+   end
+
+
 
 end
--- Review-1/spec/grades_spec.rb
+++ Review-1/spec/grades_spec.rb
@@ -5,7 +5,22 @@ describe Grades do
   describe 'input' do
     it 'Takes grades as an input' do
       grades = "Green, Green, Amber, Red, Green"
-      expect(subject.input(grades)).to equal(grades)
+      expect(subject.input(grades)).to eq(["Green", "Green", "Amber", "Red", "Green"])
     end
   end
-end
+
+  describe 'gradeCount' do 
+    it 'Takes the grades and counts them based on score' do
+      gradeArr = ["Green", "Green", "Amber", "Red", "Green"]
+      expect(subject.gradeCount(gradeArr)).to eq({"Amber"=>1, "Green"=>3, "Red"=>1})
+    end
+  end
+
+  describe 'gradeResult' do
+    it 'Takes a hash of results and returns them as string' do
+      result = {"Amber"=>1, "Green"=>3, "Red"=>1}
+      expect(subject.gradeResult(result)).to eq("Amber: 1\nGreen: 3\nRed: 1")
+      
+    end
+  end
+ end
